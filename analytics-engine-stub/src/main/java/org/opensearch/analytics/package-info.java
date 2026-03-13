c9532ab3bb364ef5ce8013132b9cf92f9bc7793d
Chen Dai <daichen@amazon.com>
2026-03-13 23:49:14 +0000

    feat: [US-001] Create analytics-engine-stub Gradle submodule

---
+++ analytics-engine-stub/src/main/java/org/opensearch/analytics/package-info.java
@@ -0,0 +1,7 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/** Analytics engine SPI interfaces and mock implementations for PoC. */
+package org.opensearch.analytics;
