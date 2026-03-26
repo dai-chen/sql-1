96c623144665a328d36bd316b8668567c0b5f881
Chen Dai <daichen@amazon.com>
2026-03-26 23:35:19 +0000

    feat: [US-002] Create cli Gradle module with dependencies

---
+++ cli/src/main/java/org/opensearch/sql/cli/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.sql.cli;
+
+/** Entry point for the OpenSearch Unified Query Shell. */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("OpenSearch Unified Query Shell");
+  }
+}
